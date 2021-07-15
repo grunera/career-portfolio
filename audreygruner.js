e4b8adabb7c34d5f1b52b8da84ef67d12ad7e99e
Audrey Gruner <66530333+grunera@users.noreply.github.com>
2021-07-15 12:03:12 +0200

    1st commit

---
+++ audreygruner.js
@@ -0,0 +1,13 @@
+<script type="text/javascript" >
+
+        const hamburger = document.querySelector(".hamburger");
+        const navMenu = document.querySelector(".nav-menu");
+
+        hamburger.addEventListener("click", mobileMenu);
+
+        function mobileMenu() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+        }
+
+    </script>
